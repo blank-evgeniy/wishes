f3539f5570c0a95f275cad8461aa6b70676de174
Blank Evgeniy <blank.eugeny@yandex.ru>
2026-02-13 10:38:26 +0200

    fix: header links

--- src/layout/base/base.tsx
+++ src/layout/base/base.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 
 import { routes } from "@/shared/routes";
-import { Button } from "@/shared/ui/button";
 import { Container } from "@/shared/ui/container";
 
-import { MobileNavigation } from "./ui/mobile-navigation";
+import { HeaderNavigation } from "./ui/header-navigation";
 
 export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
   const currentYear = new Date().getFullYear();
@@ -19,16 +18,7 @@ export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
           W<span className="absolute -left-1/4 text-primary/20">W</span>
         </Link>
 
-        <nav className="hidden sm:flex gap-4">
-          <Button variant={"ghost"} asChild>
-            <Link href={routes.dashboard}>Мои вишлисты</Link>
-          </Button>
-          <Button variant={"ghost"} asChild>
-            <Link href={routes.settings}>Настройки</Link>
-          </Button>
-        </nav>
-
-        <MobileNavigation className="sm:hidden" />
+        <HeaderNavigation />
       </header>
 
       {children}
